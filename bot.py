92382b6a770b8043982e1b4314af852cc077411e
Astrum06 <80364127+Astrum06@users.noreply.github.com>
2021-03-11 09:58:44 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telethon import TelegramClient
+import asyncio
+import time
+from telethon import events
+
+api_id = 'API ID'
+api_hash = 'API HASH'
+#destination_user_username='Astrum06'
+
+
+
+client = TelegramClient('BOT USERNAME', api_id, api_hash)
+message_default = 'Message'
+
+
+def main():
+    # Create the client and connect
+
+    client = TelegramClient('telehandler_session', api_id, api_hash)
+    client.start()
+    
+    entity=client.get_entity(destination_user_username)
+    @client.on(events.NewMessage(incoming=True))
+    async def handler(event):
+        print(time.asctime(), '-', event.message)
+        time.sleep(2)
+        await event.reply(message_default)
+    print(time.asctime(), '-', 'Waiting for incoming messages...')
+    client.run_until_disconnected()
+    print(time.asctime(), '-', 'Stopped!')
+
+
+if __name__ == '__main__':
+    main()
